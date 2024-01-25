c1c44a13a90c93ee73c5c5001aaf5d0d3263b82f
Haritiana <haritiananomenjanahary3@gmail.com>
2024-01-25 15:12:41 +0300

    Initial Upload

---
+++ src/pages/SingleEvent.jsx
@@ -0,0 +1,16 @@
+
+export default function SingleEvent() {
+  return (
+    <>
+      <div className="ctn_single_evenement h-screen bg-yellow-300">
+        <div className="ctn_presentation_evenement">
+          <div>Picturee</div>
+          <div>Information d evenemnt</div>
+        </div>
+        <div>
+          Table principale Applica
+        </div>
+      </div>
+    </>
+  )
+}
