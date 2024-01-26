b20c82f46c03391dc19d7ac6024adba0151f0fc2
Haritiana <haritiananomenjanahary3@gmail.com>
2024-01-26 12:17:11 +0300

    Creationd table

--- src/pages/SingleEvent.jsx
+++ src/pages/SingleEvent.jsx
@@ -29,7 +29,38 @@ export default function SingleEvent() {
           </div>
         </div>
         <div>
-          <h1 className="text-xl text-white">Table principale Application</h1>
+
+      <table className="table-auto">
+      <thead>
+        <tr>
+          <th className="px-4 py-2"></th>
+          <th className="border px-4 py-2">Simple</th>
+          <th className="border px-4 py-2">Silver</th>
+          <th className="border px-4 py-2">Gold</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border px-4 py-2">Intro to CSS</td>
+          <td className="border px-4 py-2">Adam</td>
+          <td className="border px-4 py-2">858</td>
+          <td className="border px-4 py-2">858</td>
+        </tr>
+        <tr className="bg-gray-100">
+          <td className="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+          <td className="border px-4 py-2">Adam</td>
+          <td className="border px-4 py-2">112</td>
+          <td className="border px-4 py-2">112</td>
+        </tr>
+        <tr>
+          <td className="border px-4 py-2">Intro to JavaScript</td>
+          <td className="border px-4 py-2">Chris</td>
+          <td className="border px-4 py-2">1,280</td>
+          <td className="border px-4 py-2">1,280</td>
+        </tr>
+      </tbody>
+    </table>
+    
         </div>
       </div>
     </>
